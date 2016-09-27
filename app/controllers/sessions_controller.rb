233a56deb2524cdad0ff16f8bb645cb55f4582da
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-27 00:17:19 +0000

    added better login verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    if User.find_by_user_id(login_params["user_id"]) == User.find_by_email(login_params["email"])
+    if (User.find_by_user_id(login_params["user_id"]) == User.find_by_email(login_params["email"])) && (User.find_by_user_id(login_params["user_id"]) != nil)
       @user = User.find_by_user_id(login_params["user_id"])
       session[:session_token] = @user.session_token
       flash[:notice] = 'Login successful'
