a65918d0e1389898c74b1ebc9839d6202d5d2128
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-27 00:01:58 +0000

    user login functionality complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
   
+  def login_params
+    params.require(:user).permit(:user_id, :email)
+  end
 
   def new
     # default: render 'new' template
   end
   
   def create
-    @user = User.find_by_user_id(:user_id)
-    session[:session_token] = @user.session_token
-    flash[:notice] = 'Login successful'
-    redirect_to movies_path
+    if User.find_by_user_id(login_params["user_id"]) == User.find_by_email(login_params["email"])
+      @user = User.find_by_user_id(login_params["user_id"])
+      session[:session_token] = @user.session_token
+      flash[:notice] = 'Login successful'
+      redirect_to movies_path
+    else
+      flash[:notice] = 'Login unsuccessful. Try again'
+      redirect_to login_path
+    end
   end
   
   def destroy
